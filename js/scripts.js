9f8b697e2e0fb79f96a11521dbe19eb61563e968
fred onyango <dyfredieto71@gmail.com>
2019-02-25 08:32:18 +0300

    fine tuning the user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
   $("#quiz").submit(function(event){
     event.preventDefault();
-    var q1 = parseInt($("[name=answer1]:checked").val());
-    var q2 = parseInt($("[name=answer2]:checked").val());
-    var q3 = parseInt($("[name=answer3]:checked").val());
-    var q4 = parseInt($("[name=answer4]:checked").val());
-    var q5 = parseInt($("[name=answer5]:checked").val());
-    var q6 = parseInt($("[name=answer6]:checked").val());
-    var q7 = parseInt($("[name=answer7]:checked").val());
-    var q8 = parseInt($("[name=answer8]:checked").val());
-    var q9 = parseInt($("[name=answer9]:checked").val());
-    var q10 = parseInt($("[name=answer10]:checked").val());
-    var q11 = parseInt($("[name=answer11]:checked").val());
-    var q12 = parseInt($("[name=answer12]:checked").val());
+    var q1 = parseInt($("[name=ans1]:checked").val());
+    var q2 = parseInt($("[name=ans2]:checked").val());
+    var q3 = parseInt($("[name=ans3]:checked").val());
+    var q4 = parseInt($("[name=ans4]:checked").val());
+    var q5 = parseInt($("[name=ans5]:checked").val());
+    var q6 = parseInt($("[name=ans6]:checked").val());
+    var q7 = parseInt($("[name=ans7]:checked").val());
+    var q8 = parseInt($("[name=ans8]:checked").val());
+    var q9 = parseInt($("[name=ans9]:checked").val());
+    var q10 = parseInt($("[name=ans10]:checked").val());
+    var q11 = parseInt($("[name=ans11]:checked").val());
+    var q12 = parseInt($("[name=ans12]:checked").val());
 
     var score = parseInt(q1+q2+q3+q4+q5+q6+q7+q8+q9+q10+q11+q12);
     if (score>70) {
